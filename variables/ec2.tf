2aa3f052761fb4eb44c481f048f4635707e837d1
gani <foraiprompt73@gmail.com>
2024-10-17 06:27:32 +0530

    terraform

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -21,14 +21,14 @@ resource "aws_security_group" "allow_ssh_terraform" {
     }
 
     tags = {
-        Name = "Allow_ssh_terra"
+        Name = "Allow_ssh_terra"  #display name in security group
     }
 }
 
 resource "aws_instance" "Terraform"{
     ami           = "ami-09c813fb71547fc4f"
     instance_type = "t2.micro"
-    vpc_security_group_ids =[aws_security_group.allow_ssh_terraform.id]
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     tags = {
     Name = "terraform"
   }
