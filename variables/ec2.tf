94d1a9d479e00509b152b6bbdf10d5242f4f211f
gani <foraiprompt73@gmail.com>
2024-10-17 06:59:50 +0530

    terraform

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,32 +1,34 @@
 resource "aws_security_group" "allow_ssh_terraform" {
 
   # ... other configuration ...
-    name = "allow_sshh" #name of the security group.
-    description = " allow port 22 for ssh connectivity." #this is optional.
+    name = var.name #name of the security group.
+    description = var.description #this is optional.
 
     egress {  #outgoing traffic 
         from_port        = 0
         to_port          = 0
         protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
+        cidr_blocks      = var.cidr_blocks
         ipv6_cidr_blocks = ["::/0"]
     }
 
     ingress {  #incoming traffic 
-        from_port        = 22
-        to_port          = 22
+        from_port        = var.from_port
+        to_port          = var.to_port
         protocol         = "tcp"
-        cidr_blocks      = ["0.0.0.0/0"]
+        cidr_blocks      = var.cidr_blocks
         ipv6_cidr_blocks = ["::/0"]
     }
 
-    tags = {
-        Name = "Allow_ssh_terra"  #display name in security group
-    }
+    tags = var.tags
+
+    # tags = {
+    #     Name = "Allow_ssh_terra"  #display name in security group
+    # }
 }
 
 resource "aws_instance" "Terraform"{
-    ami           = "ami-09c813fb71547fc4f"
+    ami           = var.ami
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     tags = {
---
+++ variables/variables.tf
@@ -0,0 +1,51 @@
+
+variable "name"{
+    default = "allow_sshh"
+    type = string
+}
+
+variable "description"{
+    default = "allow port 22 for ssh connectivity."
+    type = string
+}
+
+variable "from_port"{
+    default = 22
+    type = number
+}
+
+variable "to_port"{
+    default = 22
+    type = number
+}
+
+variable "protocol"{
+    default = "tcp"
+    type = string
+}
+
+variable "cidr_blocks"{
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
+
+variable "tags"{
+    type = map
+    default = {  #this is a map whatever possible we can declare here.
+        Name = "Allow_ssh_terra"
+        project = "Testing_terraform"
+        module = "main server"
+        Environment = "practice"
+        Terraform = "true"
+    }
+}
+
+variable "ami"{
+    type = string
+    default = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type"{
+    type = string
+    default = "t2.micro"   
+}
