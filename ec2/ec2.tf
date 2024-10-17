84618d481464397734b1bb93ad42839139ba61b1
gani <foraiprompt73@gmail.com>
2024-10-17 06:22:44 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -24,12 +24,12 @@ resource "aws_security_group" "allow_ssh_terraform" {
   
 }
 #creating a ec2 instance.
-    resource "aws_instance" "terraform_instance" {
+resource "aws_instance" "terraform_instance" {
   ami                     = "ami-09c813fb71547fc4f"
   instance_type           = "t2.micro"
   vpc_security_group_ids =[aws_security_group.allow_ssh_terraform.id]
-   tags = {
-        Name = "terraform"
+  tags = {
+    Name = "terraform"
   }
 
   }
---
+++ variables/ec2.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "allow_ssh_terraform" {
+
+  # ... other configuration ...
+    name = "allow_sshh" #name of the security group.
+    description = " allow port 22 for ssh connectivity." #this is optional.
+
+    egress {  #outgoing traffic 
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {  #incoming traffic 
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "Allow_ssh_terra"
+    }
+}
+
+resource "aws_instance" "Terraform"{
+    ami           = "ami-09c813fb71547fc4f"
+    instance_type = "t2.micro"
+    vpc_security_group_ids =[aws_security_group.allow_ssh_terraform.id]
+    tags = {
+    Name = "terraform"
+  }
+}
+
